55b64150dd684da59bb3f92062568815f0232308
abena1apaw <57848351+abena1apaw@users.noreply.github.com>
2021-01-02 12:02:43 -0600

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -57,7 +57,7 @@ router.post("/register", (req, res) => {
       
         
       if (!user) {
-        return res.status(404).json({ emailnotfound: "Email not found" });
+        return res.status(404).json({ emailnotfound: "Email not in database" });
       }
 
         
