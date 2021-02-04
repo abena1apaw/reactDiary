665d9f2fd9240b6d1efc568cd1d98b8789560080
abena1apaw <57848351+abena1apaw@users.noreply.github.com>
2021-02-03 18:07:09 -0600

    added the client side
    started the client side, login/auth.

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,3 @@
-module.exports = {
-    mongoURI: "mongodb+srv://dbuser:AItZ29azEXpRQvvm@cluster0.rlg9x.mongodb.net/mern-Diary",
-    secretOrKey: "secret"
-  };
+export const mongoURI = "mongodb+srv://dbuser:AItZ29azEXpRQvvm@cluster0.rlg9x.mongodb.net/mern-Diary";
+export const secretOrKey = "secret";
   
