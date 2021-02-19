82222a2ee24a5cb39b364364cfbae384fa6d8b17
Abena Apaw <57848351+abena1apaw@users.noreply.github.com>
2021-02-18 23:31:45 -0600

    Update login.js

--- validation/login.js
+++ validation/login.js
@@ -1,16 +1,10 @@
 const Validator = require("validator");
 const isEmpty = require("is-empty");
-module.exports = function validateRegisterData(data) {
+module.exports = function validateLoginData(data) {
   let errors = {};
 // Convert empty fields to an empty string so we can use validator functions
-  data.name = !isEmpty(data.name) ? data.name : "";
   data.email = !isEmpty(data.email) ? data.email : "";
   data.password = !isEmpty(data.password) ? data.password : "";
-  data.password2 = !isEmpty(data.password2) ? data.password2 : "";
-// Name checks
-  if (Validator.isEmpty(data.name)) {
-    errors.name = "Please enter your name";
-  }
 // Email checks
   if (Validator.isEmpty(data.email)) {
     errors.email = "Please enter your email";
@@ -20,15 +14,6 @@ module.exports = function validateRegisterData(data) {
 // Password checks
   if (Validator.isEmpty(data.password)) {
     errors.password = "Password is required";
-  }
-if (Validator.isEmpty(data.password2)) {
-    errors.password2 = "Please confirm your password";
-  }
-if (!Validator.isLength(data.password, { min: 8, max: 45 })) {
-    errors.password = "At least 8 characters are needed for the password";
-  }
-if (!Validator.equals(data.password, data.password2)) {
-    errors.password2 = "Both passwords must match";
   }
 return {
     errors,
